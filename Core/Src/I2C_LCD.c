d2153eb95ca296602ecef9a30a0a62a99d6f23af
Nirav Prajapati <Nirav0106@VERDEMOBILITY.LOCAL>
2024-04-12 17:01:08 +0530

    files added

---
+++ Core/Src/I2C_LCD.c
@@ -0,0 +1,86 @@
+
+/** Put this in the src folder **/
+
+#include "I2C_LCD.h"
+#include "main.h"
+extern I2C_HandleTypeDef hi2c1;  // change your handler here accordingly
+
+#define SLAVE_ADDRESS_LCD 0x39 // change this according to ur setup
+
+void lcd_send_cmd (char cmd)
+{
+  char data_u, data_l;
+	uint8_t data_t[4];
+	data_u = (cmd&0xf0);
+	data_l = ((cmd<<4)&0xf0);
+	data_t[0] = data_u|0x0C;  //en=1, rs=0
+	data_t[1] = data_u|0x08;  //en=0, rs=0
+	data_t[2] = data_l|0x0C;  //en=1, rs=0
+	data_t[3] = data_l|0x08;  //en=0, rs=0
+	HAL_I2C_Master_Transmit (&hi2c1, (39 << 1) ,(uint8_t *) data_t, 4, 100);
+}
+
+void lcd_send_data (char data)
+{
+	char data_u, data_l;
+	uint8_t data_t[4];
+	data_u = (data&0xf0);
+	data_l = ((data<<4)&0xf0);
+	data_t[0] = data_u|0x0D;  //en=1, rs=1
+	data_t[1] = data_u|0x09;  //en=0, rs=1
+	data_t[2] = data_l|0x0D;  //en=1, rs=1
+	data_t[3] = data_l|0x09;  //en=0, rs=1
+	HAL_I2C_Master_Transmit (&hi2c1, (39 << 1),(uint8_t *) data_t, 4, 100);
+}
+
+void lcd_init (void)
+{
+
+    // 4 bit initialisation
+    HAL_Delay(50);  // wait for >40ms
+    lcd_send_cmd (0x30);
+    HAL_Delay(5);  // wait for >4.1ms
+    lcd_send_cmd (0x30);
+    HAL_Delay(1);  // wait for >100us
+    lcd_send_cmd (0x30);
+    HAL_Delay(10);
+    lcd_send_cmd (0x20);  // 4bit mode
+    HAL_Delay(10);
+
+    // display initialisation
+	lcd_send_cmd(0x28);
+	HAL_Delay(1);
+	lcd_send_cmd(0x08);  // display on/off control
+	HAL_Delay(1);
+	lcd_send_cmd(0x01);  // clear display
+	HAL_Delay(1);
+	HAL_Delay(1);
+	lcd_send_cmd(0x06);
+	HAL_Delay(1);
+	lcd_send_cmd(0x0C);
+}
+
+void lcd_send_string (char *str)
+{
+	while (*str) lcd_send_data (*str++);
+}
+
+void lcd_clear (void)
+{
+	lcd_send_cmd(0x01);
+	HAL_Delay(2);
+}
+
+void lcd_put_cur(int row, int col)
+{
+    switch (row)
+    {
+        case 0:
+            col |= 0x80;  //0x80 | 0x00, 0x80 | 0x40, 0x80 | 0x14, 0x80 | 0x54;
+            break;
+        case 1:
+            col |= 0xC0;
+            break;
+    }
+    lcd_send_cmd (col);
+}
